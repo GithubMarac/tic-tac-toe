900b4f61c8fc32995a9ed3e141e83d20b2ea9ca6
GithubMarac <mario.probst2@gmail.com>
2024-01-22 17:41:25 +0100

    load games

--- src/Components/Games.tsx
+++ src/Components/Games.tsx
@@ -4,6 +4,18 @@ import '../CSS/ticTacToe.css';
 
 
 export default function Games() {
+  const loadGames = async () => {
+    try {
+      const response = await axiosInstance.get('/games/?limit=100');
+      console.log(response);
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+
+  loadGames();
+
+
     return (
       <>
 
